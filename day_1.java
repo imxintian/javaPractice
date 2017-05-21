9e617a2f1e92e580cff015a22f08567a2c59f30b
imxintian <1971088879@qq.com>
2017-05-21 11:55:17 +0800

    add day_1.java

---
+++ day_1.java
@@ -0,0 +1,16 @@
+package javaPrictice;
+/**
+ * 国际惯例：
+ * 用Java，敲出helloworld.
+ * 开启java新篇章7
+ * @author tiantian
+ *
+ */
+public class day_1 {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
