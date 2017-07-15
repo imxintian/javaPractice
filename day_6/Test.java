0614879426fe5ebce3e4f723f9b0376e7a5a51b8
王新天 <1971088879@qq.com>
2017-07-15 15:31:54 +0800

    Update Test.java

--- day_6/Test.java
+++ day_6/Test.java
@@ -3,7 +3,7 @@ package com.xintian.day_6;
 public class Test {
 
 	public static void main(String[] args) {
-	//´´½¨´òÓ¡»ú
+	//åˆ›å»ºæ‰“å°æœº
 		Printer p=new Printer();
 		p.setInkBox(new BlackInkBox());
 		p.setPaper(new A4Paper());
